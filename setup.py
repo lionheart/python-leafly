47175cf81744d90b516057f35b060d725d92b24b
Dan Loewenherz <dloewenherz@gmail.com>
2014-09-19 12:49:58 -0500

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -12,9 +12,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.md")) as file:
-    long_description = file.read()
-
 classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Environment :: Console",
@@ -46,7 +43,6 @@ setup(
     name='leafly',
     version=metadata.__version__,
     url="http://github.com/lionheart/python-leafly",
-    long_description=long_description,
     description="A Python wrapper for Leafly",
     classifiers=classifiers,
     keywords="leafly",
